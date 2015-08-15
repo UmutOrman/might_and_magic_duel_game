72f7ff930361c56adbddd52a7286a8ca5dba57e9
Umut Orman <umutormans@gmail.com>
2015-08-15 19:37:47 +0300

    Update effects.py

--- effects.py
+++ effects.py
@@ -1,3 +1,9 @@
+#script name: effects.py
+#author: Umut Orman
+#created: 14th August 2015
+#version: 1.0
+#description: effect class description exists in this script
+
 class Effect(object):
     def __init__(self, under_effect, counter, effect_time):
 		self.under_effect = under_effect
